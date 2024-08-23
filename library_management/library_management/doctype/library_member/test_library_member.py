e5e6af91c3857f89b6269f4b43efe2041b861537
Ajmal Roshan <ajmalroshan2002@gmail.com>
2024-08-23 10:28:43 +0530

    feat: Added a doctype library_member

---
+++ library_management/library_management/doctype/library_member/test_library_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ajmal Roshan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryMember(FrappeTestCase):
+	pass
